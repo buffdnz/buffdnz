81c3e5d85091816af64ff833c5953eb74d577e88
shifuguru <54631241+shifuguru@users.noreply.github.com>
2026-04-17 16:05:36 +1200

    Updates

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isGithubPagesBuild = process.env.GITHUB_ACTIONS === 'true';
+const repo = 'buffdnz';
+
 const nextConfig = {
   images: {
     unoptimized: true,
   },
+  ...(isGithubPagesBuild
+    ? {
+        output: 'export',
+        basePath: `/${repo}`,
+        assetPrefix: `/${repo}/`,
+        trailingSlash: true,
+      }
+    : {}),
 };
 
 module.exports = nextConfig;
